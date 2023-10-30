b4c42bea6d38a631bf79199348761eba9834ff9c
nandhini208 <145302456+nandhini208@users.noreply.github.com>
2023-10-30 16:36:23 -0400

    Account/Profile

---
+++ app/src/main/java/com/example/rentahelp/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.rentahelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+
+}
