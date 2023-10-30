f1447bbd4f01ae79101c2c92d588fb4fc1cba731
Vishvaja <53148751+Vishvaja@users.noreply.github.com>
2023-10-30 12:02:53 -0400

    Post and browsing done

--- app/src/main/java/com/example/rentahelp/ServicePostingActivity.java
+++ app/src/main/java/com/example/rentahelp/ServicePostingActivity.java
@@ -12,14 +12,18 @@ import android.widget.EditText;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.text.InputFilter;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Stack;
+
 public class ServicePostingActivity extends AppCompatActivity {
 
-    private EditText title_post;
+    static String TAG = ServicePostingActivity.class.getName();
+    private String title_post;
     private EditText description_post;
     private EditText price_post;
 
@@ -72,6 +76,7 @@ public class ServicePostingActivity extends AppCompatActivity {
                 // Handle the selected item
                 String selectedTitle = parentView.getItemAtPosition(position).toString();
                 // Do something with the selectedTitle
+                title_post = selectedTitle;
             }
 
             @Override
@@ -81,36 +86,41 @@ public class ServicePostingActivity extends AppCompatActivity {
         });
         description_post= findViewById(R.id.editTextDescription);
         price_post = findViewById(R.id.editTextPrice);
-        price_post.setFilters(new InputFilter[] {
-                new InputFilter() {
-                    @Override
-                    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-                        // Define a regular expression to match only numeric characters
-                        String regex = "^[0-9]+";
-
-                        // Check if the input matches the regular expression
-                        if (source.toString().matches(regex)) {
-                            return null; // Accept the input
-                        } else {
-                            return ""; // Reject the input (no character/string)
-                        }
-                    }
-                }
-        });
+//        price_post.setFilters(new InputFilter[] {
+//                new InputFilter() {
+//                    @Override
+//                    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+//                        // Define a regular expression to match only numeric characters
+//                        String regex = "^[0-9]+";
+//
+//                        // Check if the input matches the regular expression
+//                        if (source.toString().matches(regex)) {
+//                            return null; // Accept the input
+//                        } else {
+//                            return ""; // Reject the input (no character/string)
+//                        }
+//                    }
+//                }
+//        });
         Button postButton = findViewById(R.id.buttonPost1);
 
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(ServicePostingActivity.this, "service.getTitle()", Toast.LENGTH_SHORT).show();
                 // Get user input
-                String title = title_post.getText().toString().trim();
-                String description = description_post.getText().toString().trim();
-                double price = Double.parseDouble(price_post.getText().toString());
+                String title = title_post;
+                String description = description_post.getText().toString();
+//                double price = Double.parseDouble(price_post.getText().toString());
 
                 // Create a new service object
 //                Service service = new Service(title, description, price);
 
                 Service service = new Service();
+                service.setTitle(title);
+                service.setDescription(description);
+                service.setPrice(34);
+                Toast.makeText(ServicePostingActivity.this, service.getTitle(), Toast.LENGTH_SHORT).show();
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference servicesRef = database.getReference("Services");
 
@@ -124,7 +134,7 @@ public class ServicePostingActivity extends AppCompatActivity {
 ////                databaseReference.child(serviceId).setValue(service);
 //                servicesReference.push().setValue(service);
                 // Redirect to the service browsing activity or other actions
-//                startActivity(new Intent(ServicePostingActivity.this, MainActivity.class));
+                startActivity(new Intent(ServicePostingActivity.this, ServiceBrowsingActivity.class));
 //                finish();
             }
         });
