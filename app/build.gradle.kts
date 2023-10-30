ac770e4f825f65c327e434b8ef8d0bdb5a04246c
nandhini208 <145302456+nandhini208@users.noreply.github.com>
2023-10-30 18:40:19 -0400

    Account/Profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.firebase:firebase-database:20.0.0") // Use the latest version
-    implementation("com.google.firebase:firebase-auth:21.0.1")  // Use the latest version if using authentication
 
 }
