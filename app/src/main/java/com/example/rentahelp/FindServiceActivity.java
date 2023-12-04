24934483c64632a407e832b9db8e9cfe57c94699
Sulaiman <sulaiman@lentra.ai>
2023-12-03 20:44:24 -0500

    Added DB changes and combined Rishabh's code

--- app/src/main/java/com/example/rentahelp/FindServiceActivity.java
+++ app/src/main/java/com/example/rentahelp/FindServiceActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.rentahelp.model.Service;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
